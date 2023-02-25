7ac14603e6452199da8a7dd78e8d7ba513f05155
sydney-singlifeph <sydneydiala@singlife.com>
2023-02-25 22:11:23 +0800

    recipe blog first commit

---
+++ Client/src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div style={{ backgroundColor: "blue", borderRadius: 25 }}>Test</div>;
+}
+
+export default App;
---
+++ Client/src/main.tsx
@@ -0,0 +1,12 @@
+// Package
+import ReactDOM from "react-dom/client";
+
+// Pages
+import App from "./App";
+
+// Styles
+import "../src/Assets/css/Global.css";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <App />
+);
